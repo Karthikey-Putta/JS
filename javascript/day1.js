f83d6c04611876fead05aa6405a1138970e35d6a
karthikeya-putta <puttakarthikeya2@gmail.com>
2025-02-13 02:03:03 +0530

    Add files via upload

---
+++ javascript/day1.js
@@ -0,0 +1,87 @@
+
+
+//    task 1
+function sayHello(){
+   console.log("hello World");
+   }
+  sayHello()
+  
+  
+//    task 2
+  function displayWarning() {
+    console.log("warning! system Overloaded!");
+    }
+   displayWarning()
+//    task 3
+   function printStatus() {
+    console.log("system status : Active");
+    }
+     printStatus()
+//    task 4
+   function initilize() {
+     console.log("Initialization Complete.");
+    shutDown()
+  }
+   function shutDown () {
+   console.log("system shutting Down");
+  }
+//    task 5
+  function startProcess(){
+  console.log("Process Started");
+   endProcess()
+  }
+  function endProcess(){
+  console.log("process End");
+  }
+  startProcess()
+//    task 6
+  function greetUser(Name){
+   console.log("hello", Name,'!');
+  }
+ greetUser('karthik')
+  greetUser('shiva')
+  greetUser('mahesh')
+//    task 7
+  function calculateSum(a,b){
+  var a
+  var b 
+  console.log(a+b);
+  }
+   
+  calculateSum(2,5)
+   
+  calculateSum(3,5)
+//    task 8
+  function countDown(){
+     var i
+      for (i=5; i>=5; i--)
+  console.log(i)
+  }
+  countDown()
+//    task 9
+  function checkEvenOdd (num){
+  var num;
+  if (num%2==0)
+  console.log("Even")
+  else
+  console.log("odd")
+  
+  
+  }
+  checkEvenOdd (5)
+  checkEvenOdd (3)
+   
+//    task 10
+  function beginSession(){
+  console.log("Session Started.")
+   endSession()
+  }
+  function endSession(){
+   console.log("Session Ended")
+  }
+  beginSession()
+  
+  
+  
+  
+  
